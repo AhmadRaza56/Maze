1401a06ecd615fe6608f9322dcdebcc159ab909e
razaa <razaa@AHMAD>
2021-06-16 13:26:44 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playerImage = new System.Windows.Forms.PictureBox();
+            this.picWall = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerImage
+            // 
+            this.playerImage.Image = ((System.Drawing.Image)(resources.GetObject("playerImage.Image")));
+            this.playerImage.Location = new System.Drawing.Point(352, 534);
+            this.playerImage.Name = "playerImage";
+            this.playerImage.Size = new System.Drawing.Size(72, 62);
+            this.playerImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerImage.TabIndex = 0;
+            this.playerImage.TabStop = false;
+            // 
+            // picWall
+            // 
+            this.picWall.Image = ((System.Drawing.Image)(resources.GetObject("picWall.Image")));
+            this.picWall.Location = new System.Drawing.Point(339, 0);
+            this.picWall.Name = "picWall";
+            this.picWall.Size = new System.Drawing.Size(85, 156);
+            this.picWall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWall.TabIndex = 1;
+            this.picWall.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 608);
+            this.Controls.Add(this.picWall);
+            this.Controls.Add(this.playerImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.playerImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox playerImage;
+        private System.Windows.Forms.PictureBox picWall;
+    }
+}
+
---
+++ Form3.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Maze
+{
+    partial class frmControls
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblControls = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblControls
+            // 
+            this.lblControls.AutoSize = true;
+            this.lblControls.Font = new System.Drawing.Font("Old English Text MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblControls.Location = new System.Drawing.Point(299, 44);
+            this.lblControls.Name = "lblControls";
+            this.lblControls.Size = new System.Drawing.Size(120, 32);
+            this.lblControls.TabIndex = 0;
+            this.lblControls.Text = "Controls";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To move the character upwards press the \'W\' key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To move the character downwards press the \'S\' key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "To move the character to the right press the \'D\' key";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "To move the character to the left press the \'A\' key";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "To open the pause menu press the \'ESC\' key";
+            // 
+            // frmControls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblControls);
+            this.Name = "frmControls";
+            this.Text = "Controls";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblControls;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
